825076af7ab92a6f00a55f9557001eec2ac02b79
nikika-mir0 <nmironov327@gmail.com>
2026-04-13 18:13:24 +0500

    menu beta

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
-
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dsd");
+        Window win = new Window();
+
+        win.initWindow();
+
     }    
 }
