dc4348cbc08bd6b15720fd122c6036aacfaa79c5
rydanee <rydanee1@gmail.com>
2026-04-13 18:50:20 +0500

    socket image transfer

--- SocketManager.java
+++ SocketManager.java
@@ -2,8 +2,16 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class SocketManager {
 
@@ -42,8 +50,9 @@ public class SocketManager {
                 public void run() {
                     while (s.isConnected()) {
                         try {
-                            m = in.readUTF();
-                            System.out.println(m);
+                            BufferedImage img = ImageIO.read(s.getInputStream());
+                            ImageIO.write(img, "PNG", new File("received.png"));
+                            System.out.println(img.getHeight());
                         } catch (IOException e) {
                         }
                     }
@@ -67,8 +76,39 @@ public class SocketManager {
         public boolean stop = false;
         DataInputStream in;
         DataOutputStream out;
+        Robot robot;
+        Rectangle screenRect;
+
+        public static boolean isWayland() {
+            String sessionType = System.getenv("XDG_SESSION_TYPE");
+            return "wayland".equalsIgnoreCase(sessionType);
+        }
+
+        BufferedImage takeScreenshotGrim() {
+            try {
+                Process process = new ProcessBuilder("grim", "-").start();
+                InputStream is = process.getInputStream();
+                BufferedImage image = ImageIO.read(is);
+                process.waitFor();
+                return image;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        BufferedImage takeScreenshot() {
+            BufferedImage img = robot.createScreenCapture(screenRect);
+
+            return img;
+        }
 
         public Client(String ip, int port) {    
+            screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+            }
+
             try {
                 cs = new Socket(ip, port);
                 in = new DataInputStream(System.in);
@@ -85,8 +125,12 @@ public class SocketManager {
                 public void run() {
                     while (cs.isConnected()) {
                         try {
-                            m = in.readLine();
-                            out.writeUTF(m);
+                            BufferedImage img = null;
+                            if (isWayland()) {
+                                img = takeScreenshotGrim();
+                            } else img = takeScreenshot();
+                            ImageIO.write(img, "PNG", cs.getOutputStream());
+
                         } catch (IOException e) {
                         }
                     }
